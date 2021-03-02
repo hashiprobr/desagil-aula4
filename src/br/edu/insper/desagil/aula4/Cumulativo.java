ab0821747d3f1a0c401a413140a4c0c1e1a4e0d5
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2021-03-02 20:43:00 -0300

    Add initial files

---
+++ src/br/edu/insper/desagil/aula4/Cumulativo.java
@@ -0,0 +1,21 @@
+package br.edu.insper.desagil.aula4;
+
+import java.util.ArrayList;
+
+public class Cumulativo {
+
+	public List<Integer> calcula(List<Integer> v) {
+		List<Integer> c = new ArrayList<>();
+
+		for (int i = 0; i < v.size(); i++) {
+			int s = 0;
+			for (int j = 0; j <= i; j++) {
+				s += v.get(j);
+			}
+			c.add(s);
+		}
+
+		return c;
+	}
+
+}
